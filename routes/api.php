a6a11352c7d397d80b5ad072ae50abc34e5ca4fc
sshanto13 <sumancsediu.com>
2026-01-12 00:46:22 +0600

    added sanctum for auth and and added api route with v1 prefix

---
+++ routes/api.php
@@ -0,0 +1,8 @@
+<?php
+use App\Http\Controllers\api\AuthController;
+
+Route::prefix('v1')->group(function () {
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+});
